487ba3e62d1cf6b8d7cd5743e57cff3e712278f3
Bishwo Raut <bishowraut@gmail.com>
2020-10-22 00:58:50 +0545

    added

--- assets/js/app.js
+++ assets/js/app.js
@@ -25,45 +25,45 @@
 				$(this).siblings().toggle("active");
 			});
 
-            $('.notice-slider').slick({
-  dots: false,
-  infinite: true,
-  speed: 300,
-arrows: false,
-  autoplay: true,
-  autoplaySpeed: 1500,
-  slidesToShow: 2,
-  slidesToScroll: 1,
-  responsive: [
-    {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 1,
+      $('.notice-slider').slick({
+        dots: false,
+        arrows: false,
         infinite: true,
-        dots: true
-      }
-    },
-    {
-      breakpoint: 600,
-      settings: {
-        slidesToShow: 1,
-		arrows: false,
-        slidesToScroll: 1
-      }
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        slidesToShow: 1,
-		arrows: false,
-        slidesToScroll: 1
-      }
-    }
-    // You can unslick at a given breakpoint now by adding:
-    // settings: "unslick"
-    // instead of a settings object
-  ]
-});
-		
-	
+        speed: 300,
+        // autoplay: true,
+        autoplaySpeed: 1500,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 1,
+              infinite: true,
+              dots: true
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+          arrows: false,
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+          arrows: false,
+              slidesToScroll: 1
+            }
+          }
+          // You can unslick at a given breakpoint now by adding:
+          // settings: "unslick"
+          // instead of a settings object
+        ]
+      });
+            
+      
